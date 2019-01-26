281ae402011bfde54411a577f6916a44e7ccbb4f
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-01-26 13:40:22 -0500

    tested server request

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express()
+const fs = require('fs')
+const version = require('./package.json').version
+var serverConfig = require('./config.js').serverConfig
+const PORT = serverConfig.port || 3001
+var path = require('path')
+const launch = require('./launch.js')
+const LaunchJS = new launch()
+
+var bodyParser = require('body-parser')
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+//app.use(cookieParser())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+
+
+app.get('/rocket', function (req, res) {
+    console.log(req.body)
+
+            res.send("index")
+})
+
+app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`));
+module.exports = app
