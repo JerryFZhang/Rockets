f6bc6386dd1e3fc7e0591ac5900de30b8ae8db4f
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-03-29 19:23:27 -0400

    fixing class vs className and empty vid URLs

--- frontend/src/Vid.js
+++ frontend/src/Vid.js
@@ -9,8 +9,8 @@ class Vid extends React.Component {
 
     render() {
         let component
-        if (this.obj.vidURL) component = <a href="this.obj.vidURL" class="badge badge-primary">Watch Video</a> 
-        if (this.obj.vidURLs) component = this.obj.vidURLs.map((vid, index) => <a href="vid" class="badge badge-primary">Video Link #{index + 1}</a>)
+        if (this.obj.vidURL) component = <a href="this.obj.vidURL" className="badge badge-primary">Watch Video</a> 
+        if (this.obj.vidURLs) component = this.obj.vidURLs.map((vid, index) => <a href="{vid}" className="badge badge-primary">Video Link #{index + 1}</a>)
        return (
         <span>{component}</span>
       );
